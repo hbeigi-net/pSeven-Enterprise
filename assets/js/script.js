d7e8bc27c9da65b0061bb53799e2e5b8dcdfe6ba
hamid <hbeigi57@gmail.com>
2021-11-15 17:00:47 +0330

    header compeleted

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+$(document).ready(function () {
+    // small screen toggler 
+    $("#small_screen_nav_toggler").click(function (e) {
+        $("#small_screen_navbar").toggleClass("show");
+       
+        if ($("#small_screen_nav_toggler i").hasClass("fa-navicon")) {
+            $("#small_screen_nav_toggler i").removeClass("fa-navicon");
+            $("#small_screen_nav_toggler i").addClass("fa-times");
+        }
+        else {
+            $("#small_screen_nav_toggler i").removeClass("fa-times");
+            $("#small_screen_nav_toggler i").addClass("fa-navicon");
+        }
+    });
+
+    let animated = new Typed("#animated_title",{
+        strings:["مجهز به تیمی مجرب ^1000"," ^1000تضمین عملکرد "," ^1000استفاده از متریال درجه یک "],
+        typeSpeed : 60,
+        loop : true,
+        backSpeed: 20,
+        cursorChar: '',
+
+    })
+
+});
