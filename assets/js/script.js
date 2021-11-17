f1c8f10aaaba600168d3771c2bcddbd459aa5351
hamid <hbeigi57@gmail.com>
2021-11-17 15:16:53 +0330

    owl carousel bugs should be fixed in this branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,5 +22,33 @@ $(document).ready(function () {
         cursorChar: '',
 
     })
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:10,
+        rtl : true,
+        responsive : 
+        {
+            0:
+            {
+                items:1
+            },
+            576 : 
+            {
+                items : 2
+            },
+            768 : 
+            {
+                items : 3
+            },
+            992 : 
+            {
+                 items : 4
+            },
+            1100 : 
+            {
+                items : 5
+            }
 
+        }
+    });
 });
